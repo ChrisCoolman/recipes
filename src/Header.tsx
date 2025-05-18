a8a8260e31c27f7ef51e1647024fabb6fc47c6a9
ChrisCoolman <christopher.coleman@mcvts.org>
2025-05-18 15:38:48 -0400

    changed the name

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,7 +2,7 @@
 function Header() {
     return(
         <header className="text-center text-6xl p-10 font-bold">
-            <h1 className="text-white">Find <span className="text-orange">Recipes</span> <br></br>Without The <span className="text-orange">Hassle</span></h1>
+            <h1 className="text-white">Find <span className="text-orange">Recipes</span> <br></br>The <span className="text-orange">Easy</span> Way</h1>
         </header>
     );
 }
